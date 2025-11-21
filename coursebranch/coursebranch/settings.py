f5992dedfa4d1c5405a9c55d03c6e6600400e9d4
Rudolf Rissling <rudolfr@bu.edu>
2025-11-20 23:26:16 -0500

    Misc commits

--- coursebranch/coursebranch/settings.py
+++ coursebranch/coursebranch/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coursebranch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +66,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
 ]
 
 WSGI_APPLICATION = 'coursebranch.wsgi.application'
